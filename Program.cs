8ab1e3fb5ec75055fa41a9366379205bb5784e7b
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-12 08:23:22 +0300

    version 1.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int m = int.Parse(input[0]);
+        int n = int.Parse(input[1]);
+
+        if ((m * n) % 2 == 0)
+            Console.WriteLine("[:=[first]");
+        else
+            Console.WriteLine("[second]=:]");
+    }
+}
